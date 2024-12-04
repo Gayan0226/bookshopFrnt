8c5858c4a8b79138fe81ccffcb446e7374a58556
DMGCD <gcdisanayaka1998@gmail.com>
2024-12-04 14:54:05 +0530

    create Login Form

--- script/index.js
+++ script/index.js
@@ -1,19 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let currentSlide = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSlide = 0;
 
-    function autoTransition() {
-      const slides = document.querySelector('.slides');
-      const totalSlides = document.querySelectorAll('.slide').length;
-    
-      // Move to the next slide
-      currentSlide = (currentSlide + 1) % totalSlides;
-    
-      // Update the slide position
-      slides.style.transform = `translateX(-${currentSlide * 100}%)`;
-    }
-    
-    // Start the auto-transition
-    setInterval(autoTransition, 4000); // Change slides every 3 seconds
-    
+  function autoTransition() {
+    const slides = document.querySelector(".slides");
+    const totalSlides = document.querySelectorAll(".slide").length;
 
+    // Move to the next slide
+    currentSlide = (currentSlide + 1) % totalSlides;
+
+    // Update the slide position
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+  }
+
+  // Start the auto-transition
+  setInterval(autoTransition, 4000); // Change slides every 3 seconds
 });
